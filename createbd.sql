7c678f10dadbf97f04c943ced44371dcd50e1de9
mihailkoreshkov <mihailkoreshkov@icloud.com>
2020-04-14 12:46:20 +0000

    first commit

---
+++ createbd.sql
@@ -0,0 +1,5 @@
+create table dmik_info(
+    films_name,
+    films_date,
+    films_decsriptions
+)
---
+++ parse.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup4
+
+
+
+
+
+def parse_films_name():
+    pass 
+
+def parse_films_date():
+    pass 
+
+def parse_films_descriptions():
+    pass
+
+
