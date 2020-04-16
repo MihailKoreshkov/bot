2e3ddaa549002a4b5d51a1ef6430a4747967524c
MihailKoreshkov <mihailkoreshkov@icloud.com>
2020-04-16 12:25:06 +0000

    commit

--- parser_dmik.py
+++ parser_dmik.py
@@ -1,27 +1,26 @@
 import requests
-import dmik_db
-
+import db
 from bs4 import BeautifulSoup
 
 
 
-
 class Parser_dmik:
-    
+      
     HEADERS = {'User-Agent':
-           'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0',
-           'Accept':
-           '*/*'}
-
-
+            'Mozilla/6.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0',
+            'Accept':
+            '*/*'}
+  
+  
     def __init__(self):
         self._bsObj = self._create_bsObj()
-
-
+  
+  
     def _create_bsObj(self, url='http://dmik.ru/'):
         html_page = requests.get(url, headers=self.HEADERS)
         soup = BeautifulSoup(html_page.text, 'html.parser')
         return soup
+ 
 
     def parse_data(self, tag: str, class_: str, quality_iter=None) -> list:
         parse_obj = self._bsObj.find_all(tag, class_=class_)
@@ -38,26 +37,44 @@ class Parser_dmik:
         return data
 
 
-
-class Update:
-
-    def __init__(self):
-        self.data = self.get_dmik_data()
-
     def get_dmik_data(self):
-        title = Parser_dmik().parse_data('a', 'name')
-        date = Parser_dmik().parse_data('a', 'date')
-        description = Parser_dmik().parse_data('p', '', len(title))
+        title = self.parse_data('a', 'name')
+        date = self.parse_data('a', 'date')
+        description = self.parse_data('p', '', len(title))
         sort_data = ()
         data = []
         for i in range(len(title)):
             sort_data = (title[i], date[i], description[i])
             data.append(sort_data)
         return data
-        
+
+
+    
+  
+  
+  
+class Update_dmik:
+  
+    def __init__(self):
+        self.delete()
+        self.data = Parser_dmik().get_dmik_data()
+        self.add_to_base()
+  
+
     def add_to_base(self):
-        dmik_db.insert("dmik_info", self.data)
-        
+        db.insert("dmik_info", self.data)
+
+
+    def delete_data(self):
+        db.delete("dmik_info")
+
+    
+                 
+
+
+
+    
+
 
 
 
