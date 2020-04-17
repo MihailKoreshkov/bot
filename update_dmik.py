0aee6441d89e3a96eaeb5a507b2ff02fa04c82fb
MihailKoreshkov <Mihailkoreshkov@icloud.com>
2020-04-18 00:20:02 +0300

    update

--- update_dmik.py
+++ update_dmik.py
@@ -7,12 +7,14 @@ class Update_dmik:
   
     def __init__(self):
         self.delete_data()
-        self.data = Parser_dmik().get_dmik_data()
+        self.anons = Parser_dmik().get_dmik_anons()
+        self.films_to_day = Parser_dmik().get_dmik_films_to_day()
         self.add_to_base()
   
 
     def add_to_base(self):
-        dmik_db.insert("dmik_info", self.data)
+        dmik_db.insert("dmik_info", self.anons)
+        dmik_db.insert("dmik_day_info",)
 
 
     def delete_data(self):
