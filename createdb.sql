c4e8664726cb746361926240a6eec8f3dbbe7f7a
MihailKoreshkov <Mihailkoreshkov@icloud.com>
2020-04-25 15:07:29 +0300

    добавлен столбец в таблицу dmik_day_info

--- createdb.sql
+++ createdb.sql
@@ -2,10 +2,11 @@ create table dmik_info(
     films_title VARCHAR(255),
     films_date VARCHAR(255),
     films_decsriptions VARCHAR(255)
-)
+);
 
 
 create table dmik_day_info(
+    calend_number INTEGER,
     day_number VARCHAR(255),
     shedule VARCHAR(255)
-)
+);
